c691711ca7e1a1132d018048707cda8981acbe53
Akshayj2005 <bbpsgrakshayj2005@gmail.com>
2026-02-02 09:37:25 +0530

    my php codes till 31-1-26

---
+++ constant.php
@@ -0,0 +1,8 @@
+<?php
+#contants 
+define("greeting", "hello world!",True);
+echo "GREETING";
+echo "<br>";
+echo greeting;
+echo "This Program Is Written And Executed By Akshay Jain 0231BCA222";
+?>
---
+++ func-rsort.php
@@ -0,0 +1,7 @@
+<?php
+#rsort function
+
+$colors = array("Red", "Green", "Blue", "Yellow");
+rsort($colors);
+print_r($colors);
+?>
