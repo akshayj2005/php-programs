fedfedb90c9b16a9edfd21a807c30bcd237d8e33
Akshayj2005 <bbpsgrakshayj2005@gmail.com>
2026-02-03 10:56:21 +0530

    3-2-26

--- func-recursive.php
+++ func-recursive.php
@@ -1,4 +1,14 @@
 <?php
+#recursive function to calculate multiplication of number by itself 
+function multiply(&$number) {
+ $number *= $number;
+ return $number;
+}
+$mynum = 5;
+echo $mynum . "<br>";
+
+multiply($mynum);
+echo $mynum;
 echo '<hr>
 <pre style="text-align:center;">
 ====================================================================
